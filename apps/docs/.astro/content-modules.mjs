2658d3c938460a6b58bbf9a2a8627a013ed64df1
Barry Michael Doyle <barry@barrymichaeldoyle.com>
2026-04-26 16:33:34 +0200

    fixed git astro issues

--- apps/docs/.astro/content-modules.mjs
+++
@@ -1,8 +0,0 @@
-
-export default new Map([
-["src/content/docs/api/components.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapi%2Fcomponents.mdx&astroContentModuleFlag=true")],
-["src/content/docs/api/hooks.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapi%2Fhooks.mdx&astroContentModuleFlag=true")],
-["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/getting-started.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started.mdx&astroContentModuleFlag=true")],
-["src/content/docs/api/event-bus.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapi%2Fevent-bus.mdx&astroContentModuleFlag=true")]]);
-		
